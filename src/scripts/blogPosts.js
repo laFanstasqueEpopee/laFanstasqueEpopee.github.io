9beaeec895bd54c94d6a7bad04a70707c67465d1
Thomas Gandou <thomas.gandou@ik.me>
2025-06-20 13:54:17 -0600

    Poursuite tutoriel Astro

--- src/scripts/blogPosts.js
+++ src/scripts/blogPosts.js
@@ -1,3 +1,13 @@
 const allPosts = Object.values(import.meta.glob('../pages/posts/*.md', { eager: true }));
-console.log(allPosts[0].frontmatter.title)
-// Trouver moyen creer html en JS (marche dans les autres cas car en TypeScript)
+const blogList = document.getElementById("blogPosts");
+allPosts.forEach(post => {
+    const listElement = document.createElement('li');
+    const linkElement = document.createElement('a');
+    linkElement.setAttribute('href',post.url)
+    linkElement.innerHTML = `${post.frontmatter.title} by ${post.frontmatter.author} 
+    (${post.frontmatter.pubDate.toString().slice(0,10)})`;
+    listElement.appendChild(linkElement);
+    blogList.appendChild(listElement);
+});
+//impossible de créer des components astro depuis un script JS
+//Finalement, script inutilisé, je passe par les components dans le layout des blogs
